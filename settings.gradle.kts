bccb74ebd6c4267dc5ee919a2a21783dc875d359
axuan <a80259@gmail.com>
2025-05-19 23:18:00 +0800

    refactor: simplify project module inclusion in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,8 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
 rootProject.name = "clean-architecture-implementation"
 include("api")
 include("jpa")
-include("use-case")
-include("core")
-include("core:domain")
-findProject(":core:domain")?.name = "domain"
-include("core:use-case")
-findProject(":core:use-case")?.name = "use-case"
-include("core:port-in")
-findProject(":core:port-in")?.name = "port-in"
+include("core:use-case", "core:domain")
